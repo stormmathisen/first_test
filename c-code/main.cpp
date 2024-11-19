d547906b3d8a8076ced4a1f3159133de1e39c62d
Storm <storm.mathisen@stfc.ac.uk>
2024-11-19 22:43:27 +0000

    Initial;

---
+++ c-code/main.cpp
@@ -0,0 +1,8 @@
+#include "first_test.h"
+
+int main() {
+  int32_t a = 1;
+  int32_t b = 2;
+  int32_t c = add(a, b);
+  printf("a + b = %d\n", c);
+}
---
+++ first_test.h
@@ -0,0 +1,14 @@
+#include <cstdarg>
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <ostream>
+#include <new>
+
+
+
+extern "C" {
+
+int32_t add(int32_t a, int32_t b);
+
+}  // extern "C"
